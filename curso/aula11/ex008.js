38184e7d015f8536c877b2f8162e5a28b166fcc0
Alisson Araújo Mariano <157244142+Alissonaraujodev@users.noreply.github.com>
2024-02-16 17:52:15 -0300

    estudando condições JS

---
+++ curso/aula11/ex008.js
@@ -0,0 +1,6 @@
+let vel = 60
+console.log (`A velocidade do seu carro é de ${vel} Km/h`)
+if (vel > 60){
+    console.log('Você ultrapassou a velocidade maxima permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurença!')
