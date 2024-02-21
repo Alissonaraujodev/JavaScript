03c3199b0be10fab221e14c2eb5cfe1e44dc6903
Alisson Araújo Mariano <157244142+Alissonaraujodev@users.noreply.github.com>
2024-02-21 16:09:31 -0300

    array

---
+++ curso/aula15/ambiente.js
@@ -0,0 +1,12 @@
+let num = [1,3,5,7,11]
+num.push(10)
+num.sort()
+let pos = num.indexOf(7)
+console.log(pos)
+//console.log(num)
+/*for(let pos = 0; pos < num.length; pos ++){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}*/
+/*for( let pos in num){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}*/
